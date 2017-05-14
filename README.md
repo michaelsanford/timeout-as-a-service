ce50ebb9c49fedaab41662198aad088923d7358f
Michael Sanford <michaelsanford@users.noreply.github.com>
2017-05-14 15:41:23 -0400

    Write the API. Fixes #1

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # timeout-as-a-service
 Need this for a Pen
+
+# Use
+
+Call the API with your choice of `GET`, `PUT`, `POST`, `DELETE`, a timeout in miliseconds, and a status code you want returned:
+
+```
+http://timeout-as-a-service.heroku.com/<timeout-in_miliseconds>/<status_code>
+```
+
+## So, like:
+
+[http://timeout-as-a-service.heroku.com/2000/418](http://timeout-as-a-service.heroku.com/2000/418)
+
+# License
+
+MIT
---
+++ index.js
@@ -0,0 +1,39 @@
+var restify = require('restify');
+
+wait = (req, res, next) => {
+    let waitTime = parseInt(req.params.time, 10);
+    let status = parseInt(req.params.status, 10);
+
+    res.header('Content-Type', 'application/json');
+    res.charSet('utf-8');
+
+    if (typeof waitTime === "number" && typeof status === "number" && waitTime !== null && status !== null) {
+        setTimeout(() => {
+            res.send(status, {waitTime, status});
+            next();
+        }, waitTime)
+    } else {
+        error(req, res, next);
+    }
+}
+
+error = (req, res, next) => {
+    res.send(500, {error: 'Invalid Request Format: /timeout_in_miliseconds/status_code'});
+    next();
+}
+
+var server = restify.createServer();
+
+server.pre(restify.pre.sanitizePath());
+
+server.get('/:time/:status', wait);
+server.put('/:time/:status', wait);
+server.post('/:time/:status', wait);
+server.del('/:time/:status', wait);
+server.head('/:time/:status', wait);
+
+server.get('/', error);
+
+server.listen(8080, () => {
+    console.log(`${server.name} online at ${server.url}`);
+});
