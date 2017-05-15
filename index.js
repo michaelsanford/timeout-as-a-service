a3bdd351b7cf86bc1f7c78b65be7e8a9c773fac2
Michael Sanford <michaelsanford@users.noreply.github.com>
2017-05-15 10:12:24 -0400

    Allow CORS *. Fixes #4

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ error = (req, res, next) => {
 var server = restify.createServer();
 
 server.use(restify.CORS({
-    origins: ['https://s.codepen.io'],
+    origins: ['*'],
 }));
 
 server.pre(restify.pre.sanitizePath());
