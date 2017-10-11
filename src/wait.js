cd13c72dfa141c02661e5945314d5b76c5f86ed3
Eric Adamski <ericadamski@users.noreply.github.com>
2017-10-11 16:27:52 -0400

    Unit tested error and wait functions (#23)
    * Unit tested error and wait functions
    
    * update node version for travis

---
+++ src/wait.js
@@ -0,0 +1,25 @@
+const MAX_WAIT_TIME = process.env.MAX_WAIT_TIME || 90000;
+
+const error = require('./error');
+
+module.exports = function wait(req, res, next) {
+    let waitTime = parseInt(req.params.time, 10);
+    let status = parseInt(req.params.status, 10);
+
+    // Sanity check so Heroku doesn't complain.
+    if (waitTime > MAX_WAIT_TIME) waitTime = MAX_WAIT_TIME;
+
+    if (
+        typeof waitTime === 'number' &&
+        typeof status === 'number' &&
+        !Number.isNaN(waitTime) &&
+        !Number.isNaN(status)
+    ) {
+        setTimeout(() => {
+            res.status(status).json({ waitTime, status });
+            next();
+        }, waitTime);
+    } else {
+        error(req, res, next);
+    }
+};
